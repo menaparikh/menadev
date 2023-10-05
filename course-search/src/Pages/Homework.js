48760f852656c1957ce9b4c5a1b3e82fcc5453cf
Mena <menaliparikh@gmail.com>
2023-10-04 20:07:05 -0400

    Working on math choices

--- course-search/src/Pages/Homework.js
+++ course-search/src/Pages/Homework.js
@@ -1,6 +1,6 @@
 // hint: reference Main.js and Main.css, as well as CourseSquare.js if you're stuck
 import React, { useState, useEffect } from 'react';
-import '../Styles/Main.css';
+import '../Styles/Homework.css';
 import Subcourse from '../Components/Subcourse';
 
 function Homework() {
@@ -9,8 +9,13 @@ function Homework() {
       <h1 className="homework-header-text">Course Search</h1>
       <div className="subcourses">
         {/* Put the subcourses here */}
-        <Subcourse courseName={"trigonometry"}/>
-      </div>
+        <Subcourse courseName={"Trigonometry"}/>
+        <Subcourse courseName={"Algebra I"}/>
+        <Subcourse courseName={"Algebra II"}/>
+        <Subcourse courseName={"Algebra III"}/>
+        <Subcourse courseName={"Calculus AB"}/>
+        <Subcourse courseName={"Calculus BC"}/>
+       </div>
     </div>
   );
 }
