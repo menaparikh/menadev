6b1bb1e75a4d4e6596d6f47cad6ca3a647dd835b
Nalu Concepcion <lac327@cornell.edu>
2023-11-08 16:53:57 -0800

    adding navigation changes

--- course-search/src/Components/CourseSquare.js
+++ course-search/src/Components/CourseSquare.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../Styles/ComponentStyles/CourseSquare.css";
 
-function CourseSquare({ courseName }) {
+function CourseSquare({ courseName, to }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(to);
+  }
+
   return (
-    <div className="course-item course-category-button">
+    <div className="course-item course-category-button" onClick={handleClick}>
         <p className="course-name-text">{courseName}</p>
     </div>
   );
--- course-search/src/Pages/Main.js
+++ course-search/src/Pages/Main.js
@@ -10,11 +10,11 @@ function Main() {
       <h1 className="main-header-text">Course Search</h1>
       <div className="courses">
         <CourseSquare courseName={"math"} />
-        <CourseSquare courseName={"science"} />
+        <CourseSquare courseName={"science"} to="/subcourse/science"/>
         <CourseSquare courseName={"history"} />
         <CourseSquare courseName={"language"} />
         <CourseSquare courseName={"computer science"} />
-        <CourseSquare courseName={"art"} />
+        <CourseSquare courseName={"art"} to="/subcourse/art"/>
       </div>
     </div>
   );
