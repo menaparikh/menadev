194fb3cf8e64b8c6a8e5889a2b5d5c7cc05846b7
Nalu Concepcion <lac327@cornell.edu>
2023-11-19 15:18:12 -0600

    fixed layout

--- course-search/src/Pages/Main.js
+++ course-search/src/Pages/Main.js
@@ -7,14 +7,16 @@ function Main() {
   return (
     <div className="main-parent">
       <Sidebar/>
-      <h1 className="main-header-text">Course Search</h1>
-      <div className="courses">
-        <CourseSquare courseName={"math"} to="/menadev/subcourse/math/" />
-        <CourseSquare courseName={"science"} to="/menadev/subcourse/science/"/>
-        <CourseSquare courseName={"history"} to="/menadev/subcourse/history/"/>
-        <CourseSquare courseName={"language"} to="/menadev/subcourse/language/" />
-        <CourseSquare courseName={"computer science"} to="/menadev/subcourse/computer-science/" />
-        <CourseSquare courseName={"art"} to="/menadev/subcourse/art"/>
+      <div className="main-content">
+        <h1 className="main-header-text">Course Search</h1>
+        <div className="courses">
+          <CourseSquare courseName={"math"} to="/menadev/subcourse/math/" />
+          <CourseSquare courseName={"science"} to="/menadev/subcourse/science/"/>
+          <CourseSquare courseName={"history"} to="/menadev/subcourse/history/"/>
+          <CourseSquare courseName={"language"} to="/menadev/subcourse/language/" />
+          <CourseSquare courseName={"computer science"} to="/menadev/subcourse/computer-science/" />
+          <CourseSquare courseName={"art"} to="/menadev/subcourse/art"/>
+        </div>
       </div>
     </div>
   );
