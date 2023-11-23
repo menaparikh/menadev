a8dfb9c69d57ef7bf14e558430da5e2211462c60
Nalu Concepcion <lac327@cornell.edu>
2023-11-22 18:58:48 -0600

    modal popup implemented

--- course-search/src/Components/Subcourse.js
+++ course-search/src/Components/Subcourse.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
+
+import SubcourseDescription from './SubcourseDescription';
 import '../Styles/ComponentStyles/Subcourse.css';
 
+/* put another parameter in subcourse for the description and display it in the modal*/
 function Subcourse({ courseName, barColor }) {
+  const [isActive, setIsActive] = useState(false);
   const [backgroundColor, setBackgroundColor] = useState(barColor);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // set isActive to true when we click the subcourse
+  const handleSubcourseClick = () => {
+    setIsActive(!isActive);
+    setIsModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setIsActive(false);
+  };
 
   return (
-    <div
-      className="sub-course-item"
-      style={{ backgroundColor }}
-    >
-      <p className="sub-course-name-text">{courseName}</p>
+    <div>
+      <div
+        className={`sub-course-item ${isActive ? 'active' : ''}`}
+        style={{ backgroundColor: barColor }}
+        onClick={handleSubcourseClick}
+      >
+        <p className="sub-course-name-text">{courseName}</p>
+      </div>
+      {isModalOpen && (
+        <SubcourseDescription courseName={courseName} onClose={closeModal} />
+      )}
     </div>
   );
 }
---
+++ course-search/src/Components/SubcourseDescription.js
@@ -0,0 +1,20 @@
+// Modal.js
+import React from 'react';
+import '../Styles/ComponentStyles/SubcourseDescription.css';
+
+/* HW: add another parameter for the description,
+reference courseName for how to structure the 
+data flow from Subcourse -> Subcourse Description */
+function SubcourseDescription({ courseName, onClose }) {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <h2>{courseName}</h2>
+        {/* Add other modal content here */}
+        <button onClick={onClose}>Close Modal</button>
+      </div>
+    </div>
+  );
+}
+
+export default SubcourseDescription;
