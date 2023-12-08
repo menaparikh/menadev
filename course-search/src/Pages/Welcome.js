c16f5b41d0e1c08a0f25b0a8a027a9b1a297cd54
Nalu Concepcion <lac327@cornell.edu>
2023-12-08 15:31:19 -0800

    styling welcome page

--- course-search/src/Pages/Welcome.js
+++ course-search/src/Pages/Welcome.js
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 import girl from '../Assets/girl-studying.jpg';
 import '../Styles/Welcome.css';
 
-import Sidebar from '../Components/Sidebar';
-
-
 function Welcome() {
     return (
         <div className="welcome-parent">
@@ -19,13 +16,16 @@ function Welcome() {
                         <p>Course Search is a location where students can go to find courses that
                             interest them. It is designed to let you set the keywords for the
                             courses you want and it generates the best matches.</p>
-                        <h1>Why is Course Search useful?</h1>
+                        <h2>Why is Course Search useful?</h2>
                         <p>Course Search is useful since there is an abundance of
                             classes/electives. This website is more efficient and effective
                             than leafing through a course book. It also gets a more in-depth
                             explanation of each course.</p>
                     </div>
-                    <img src={girl} />
+                    <img src={girl} style={{
+                        width: '75%',
+                        height: '40%'
+                    }}/>
                 </div>
             </div>
         </div>
